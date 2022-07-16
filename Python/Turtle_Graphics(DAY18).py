f645478cc2e39fc82723cb47426779ac59a4f343
Denis <drop3x25@gmail.com>
2022-07-16 20:44:32 +0300

    feat: day18 project/challenge

---
+++ Python/Turtle_Graphics(DAY18).py
@@ -0,0 +1,29 @@
+import turtle
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("green")
+
+
+# Drawing a square
+turtle.forward(100)
+turtle.lt(90)
+turtle.forward(100)
+turtle.lt(90)
+turtle.forward(100)
+turtle.lt(90)
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
