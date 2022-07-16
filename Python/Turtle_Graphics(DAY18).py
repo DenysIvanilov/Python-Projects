b9a71db5fe87b70415c9595bf415ffa7bd83baa9
Denis <drop3x25@gmail.com>
2022-07-16 21:40:15 +0300

    feat: day18 project/challenge

--- Python/Turtle_Graphics(DAY18).py
+++ Python/Turtle_Graphics(DAY18).py
@@ -17,9 +17,9 @@ for _ in range(4):
 timmy.clear()
 # Drawing a Dashed Line
 for _ in range(15):
-    timmy.fd(10)
+    timmy.fd(5)
     timmy.penup()
-    timmy.fd(10)
+    timmy.fd(5)
     timmy.pendown()
 
 timmy.clear()
@@ -30,6 +30,23 @@ for i in range(3, 11):
     for _ in range(i):
         timmy.rt(angle)
         timmy.fd(100)
+
+timmy.clear()
+# Random Walk
+directions = [0, 90, 180, 270]
+timmy.pensize(10)
+timmy.speed(0)
+for _ in range(200):
+    timmy.color(random.choice(colours))
+    timmy.fd(15)
+    timmy.setheading(random.choice(directions))
+
+
+
+
+
+
+
 
 
 screen = Screen()
