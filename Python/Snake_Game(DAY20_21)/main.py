77cbd47bf080a2baa4ab3c5a9a8e7b7e1d7117c3
Denis <drop3x25@gmail.com>
2022-07-18 19:27:08 +0300

    feat: day20_21 project

--- Python/Snake_Game(DAY20_21)/main.py
+++ Python/Snake_Game(DAY20_21)/main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 
@@ -23,6 +25,11 @@ while game_is_on:
     time.sleep(0.05)
 
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 
 
