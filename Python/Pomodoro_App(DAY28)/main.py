ea5a3e1307b86bc49791531f5cb2b25667660c31
Denis <drop3x25@gmail.com>
2022-07-25 17:43:05 +0300

    feat: day28 project

--- Python/Pomodoro_App(DAY28)/main.py
+++ Python/Pomodoro_App(DAY28)/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,31 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+checkmark = "✔"
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
+title_label.grid(column=1, row=0)
+
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=6)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=6)
+
+checkmark_label = Label(text=checkmark, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15))
+checkmark_label.grid(column=1, row=7)
+
+
+
+window.mainloop()
