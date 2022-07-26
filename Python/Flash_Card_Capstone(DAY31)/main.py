d0c973a55589f75cdbb492a1de468c0195a989a8
Denis <drop3x25@gmail.com>
2022-07-26 14:56:53 +0300

    feat: day31 capstone project

--- Python/Flash_Card_Capstone(DAY31)/main.py
+++ Python/Flash_Card_Capstone(DAY31)/main.py
@@ -10,6 +10,7 @@ words_dict = words_df.to_dict(orient="records")
 
 def get_word():
     word = choice(words_dict)
+    canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_word, text=word["French"])
 
 
@@ -21,8 +22,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -33,5 +34,7 @@ unknown_button.grid(row=1, column=0)
 check_image = PhotoImage(file="images/right.png")
 know_button = Button(image=check_image, highlightthickness=0, command=get_word)
 know_button.grid(row=1, column=1)
+
+get_word()
 
 window.mainloop()
