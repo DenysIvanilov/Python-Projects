b57a701e7afdd69911f72890bc54b25ee98e7ddd
Denis <drop3x25@gmail.com>
2022-07-26 14:55:07 +0300

    feat: day31 capstone project

--- Python/Flash_Card_Capstone(DAY31)/main.py
+++ Python/Flash_Card_Capstone(DAY31)/main.py
@@ -1,16 +1,16 @@
 from tkinter import *
 import pandas as pd
-from random import randint, choice
+from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 # **************************** Pick a Word ****************************
 words_df = pd.read_csv("data/french_words.csv")
-words_dict = words_df.to_dict()
+words_dict = words_df.to_dict(orient="records")
 
 
 def get_word():
-    word = choice(words_dict["French"])
-    canvas.itemconfig(fr_word, text=word)
+    word = choice(words_dict)
+    canvas.itemconfig(card_word, text=word["French"])
 
 
 # **************************** UI Setup ****************************
@@ -22,7 +22,7 @@ canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
 canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-fr_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
